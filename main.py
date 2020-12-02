ee26ed3e67b0eabd10374a3d049d841b24064fd5
lmgriffin13 <71834120+lmgriffin13@users.noreply.github.com>
2020-12-02 11:16:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ st.sidebar.markdown('''Here, you can to select the final year which will be cons
 last_year = st.sidebar.slider("Select the Last Year", first_year, 2020)
 race = st.sidebar.selectbox("Here you may select the race of the players you would like to look at", ['armenian', 'asian', 'black', 'black/asian', 'hispanic', 'latinx', 'middle eastern', 'pacific islander', 'portuguese', 'unknown', 'white'])
 gender = st.sidebar.selectbox("Here you may select the gender of the players you would like to look at", ['male', 'female'])
-vetos = st.sidebar.slider('Select the number of vetos these players have won', 0, 5)
-hoh = st.sidebar.slider('Select the number of head of households these players have won', 0, 5)
+vetos = st.sidebar.slider('Select the minimum number of vetos these players have won', 0, 5)
+hoh = st.sidebar.slider('Select the minimum number of head of households these players have won', 0, 5)
 st.title(f'Big Brother Players Between {first_year} and {last_year}')
 just_these_years = (df.year >= first_year) & (df.year <= last_year)
 just_this_race = df.oringal_race_ethnicity == race
@@ -25,4 +25,11 @@ if len(focus) >0:
   st.write(focus[['name', 'final_placement','brand', 'brand_season', 'year', 'age', 'final_eviction_day', 'total_vetos', 'total_hoh', 'total_nominations', 'oringal_race_ethnicity']].reset_index( drop=True ))
 else:
   st.write("No houseuest fulfill those requirements, adjust and try again.")
-
+import matplotlib.pyplot as plt
+plt.hist(focus[['final_placement']], bins = 17, rwidth = .8)
+plt.gcf().set_size_inches(8,10)
+plt.title( f'Final Place of {gender} {race} Houseguests between {first_year} and {last_year}', fontsize=20 )
+plt.xticks( range(1,17) )
+plt.ylabel( 'Frequency', fontsize=14 )
+plt.xlabel( 'Place' )
+st.pyplot()
