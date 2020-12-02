f951bf59548e354a10af91abf55e4ef87c22e103
lmgriffin13 <71834120+lmgriffin13@users.noreply.github.com>
2020-12-02 10:54:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,7 @@ st.sidebar.markdown('''Here, you have the option to select the earliest year of 
 first_year = st.sidebar.slider("Select the First Year", 2000, 2020)
 st.sidebar.markdown('''Here, you can to select the final year which will be considered''')
 last_year = st.sidebar.slider("Select the Last Year", first_year, 2020)
-st.sidebar.markdown('''Here, you have the option to select the race of the players you will look at which will be considered in your baseball data''')
-race = st.sidebar.selectbox("Here you may select the race of the players you would like to look at", ['<select>','armenian', 'asian', 'black', 'black/asian', 'hispanic', 'latinx', 'middle eastern', 'pacific islander', 'portuguese', 'unknown', 'white'])
-st.sidebar.markdown('''Here, you may select the gender of players to consider''')
+race = st.sidebar.selectbox("Here you may select the race of the players you would like to look at", ['armenian', 'asian', 'black', 'black/asian', 'hispanic', 'latinx', 'middle eastern', 'pacific islander', 'portuguese', 'unknown', 'white'])
 gender = st.sidebar.selectbox("Here you may select the gender of the players you would like to look at", ['male', 'female'])
 vetos = st.sidebar.slider('Select the number of vetos these players have won', 0, 5)
 hoh = st.sidebar.slider('Select the number of head of households these players have won', 0, 5)
@@ -15,8 +13,8 @@ st.title(f'Big Brother Players Between {first_year} and {last_year}')
 just_these_years = (df.year >= first_year) & (df.year <= last_year)
 just_this_race = df.oringal_race_ethnicity == race
 just_this_gender = df.gender == gender
-this_many_vetos = df.total_vetos <= vetos
-this_many_hoh = df.total_hoh <= hoh
+this_many_vetos = df.total_vetos >= vetos
+this_many_hoh = df.total_hoh >= hoh
 focus = df[just_these_years & just_this_race & just_this_gender & this_many_vetos & this_many_hoh]
 
 # Which years do we care about?
